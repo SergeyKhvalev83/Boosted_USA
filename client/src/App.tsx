73ef8139d568779680e54917e69708afa551b3dc
SergeyKhvalev83 <khvalevelbrus@gmail.com>
2024-10-04 17:59:11 -0400

    Inventory statment component added

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,24 +3,24 @@ import "./App.css";
 import LayoutComponent from "./components/LayoutComponent/LayoutComponent";
 import WelcomeComponent from "./components/WelcomeComponent/WelcomeComponent";
 import BrandsComponent from "./components/BrandComponent/BrandsComponent";
+import PromiseInvComponent from "./components/PromiseInvComponent/PromiseInvComponent";
 
 function App() {
   return (
     <>
-   <LayoutComponent />
-   <WelcomeComponent />
-    <BrandsComponent />
-   
+      <LayoutComponent />
+      <WelcomeComponent />
+      <BrandsComponent />
+      <PromiseInvComponent />
     </>
   );
 }
 
 export default App;
 
-
-  // <Routes>
-    //   <Route path="/" element={<LayoutComponent />}>
-    //     <Route path="/" element={<WelcomeComponent />} />
-    //     <Route path="/" element={<BrandComponent />} />
-    //   </Route>
-    // </Routes>
+// <Routes>
+//   <Route path="/" element={<LayoutComponent />}>
+//     <Route path="/" element={<WelcomeComponent />} />
+//     <Route path="/" element={<BrandComponent />} />
+//   </Route>
+// </Routes>
---
+++ client/src/components/PromiseInvComponent/PromiseInvComponent.tsx
@@ -0,0 +1,26 @@
+import "./promiseInv.css";
+
+const PromiseInvComponent = () => {
+  return (
+    <div className="promise-inv-main-container center">
+      <div className="promiseinv-inner-container">
+        <div className="question">
+          Looking for Boosted Boards, or Boosted Revs?
+        </div>
+        <div className="inv-text">
+          Boosted USA acquired all of the remaining inventory directly from
+          Boosted. This means we have the electric skateboards and scooter you
+          all love and have been looking for. Get your hands on these highly
+          sought after products while supplies last.
+        </div>
+        {/* <div className="gray-line-container"> */}
+        {/* </div> */}
+        <div className="gray-line"></div>
+    
+
+      </div>
+    </div>
+  );
+};
+
+export default PromiseInvComponent;
